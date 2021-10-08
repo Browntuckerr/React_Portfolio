15505882ad40f3c490fb988a083b101d131bd999
Browntuckerr <82062612+Browntuckerr@users.noreply.github.com>
2021-10-08 13:10:30 -0400

    Add files via upload

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,7 +7,7 @@ const Portfolio = ()=>{
             </h1>
             <Project title="DealSella" gitHubLink="https://github.com/Browntuckerr/SteamAPIproject" deployedLink="https://browntuckerr.github.io/SteamAPIproject/"/>
             <Project title="FullStackWorkout" gitHubLink="https://github.com/Browntuckerr/FullStackWorkout" deployedLink="https://github.com/Browntuckerr/FullStackWorkout"/>
-            <Project title ="Supreme Booking" gitHubLink="https://github.com/Aswebilius/supreme-booking" deployedLink= "https://project3-supreme-bookings.herokuapp.com/"/>
+            <Project title ="social media app" gitHubLink="https://github.com/Aswebilius/celebrations" deployedLink= "https://aswebilius-chapters.herokuapp.com/posts"/>
         </div>
     )
 
